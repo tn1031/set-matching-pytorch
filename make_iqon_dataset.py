17b9da13af7179a454f8cf9aef9eaf2ad953b33e
tn1031 <ttt.nakamura1031@gmail.com>
2021-09-22 09:40:20 +0000

    change dataset format

--- make_iqon_dataset.py
+++ make_iqon_dataset.py
@@ -17,21 +17,24 @@ def validate_imagefile(path):
 
 
 def make_fitb_problems(test_sets, n_candidates):
-    all_items = set()
+    all_items = {}
     for _set in test_sets:
-        all_items = all_items | set(_set["items"])
-    all_items = list(all_items)
+        for item in _set["items"]:
+            if item["name"] not in all_items:
+                all_items[item["name"]] = item
+    item_names = list(all_items.keys())
 
     test = []
     for _set in tqdm(test_sets):
         items = _set["items"]
         pos = random.randrange(len(items))
-        answer = [items[pos]]
+        answer = {items[pos]["name"]: items[pos]}
 
         while len(answer) < n_candidates:
-            cand = random.choice(all_items)
+            cand = random.choice(item_names)
             if cand not in answer:
-                answer.append(cand)
+                answer[cand] = all_items[cand]
+        answer = list(answer.values())
 
         test.append({"question": items, "blank_position": pos, "answer": answer})
 
@@ -51,7 +54,8 @@ def split_itemsets(sets, indices):
         x_items.extend(list(items[xy_mask]))
         y_items.extend(list(items[~xy_mask]))
 
-    x_items, y_items = list(set(x_items)), list(set(y_items))
+    x_items = list({item["name"]: item for item in x_items}.values())
+    y_items = list({item["name"]: item for item in y_items}.values())
     random.shuffle(x_items)
     random.shuffle(y_items)
     return x_items, y_items
@@ -93,13 +97,26 @@ def make_finbs_problems(test_sets, n_candidates, n_mix, max_set_size_x=8, max_se
     return test
 
 
-def main(args):
-    random.seed(1219)
+def load_shift15m(args):
+    root = pathlib.Path(args.data_dir)
 
-    train_ratio, test_ratio = args.train_size, args.test_size
-    valid_ratio = 1 - (train_ratio + test_ratio)
-    assert train_ratio + valid_ratio + test_ratio == 1
+    iqon_all = []
+    for _set in json.load(open(root / args.target / "iqon_outfits.json")):
+        items = []
+        for item in _set["items"]:
+            items.append(
+                {
+                    "name": item["item_id"],
+                    "path": str(root / args.target / "features" / f"{item['item_id']}.json.gz"),
+                    "category": item["category_id1"],
+                }
+            )
+        iqon_all.append({"set_id": _set["set_id"], "items": items})
 
+    return iqon_all
+
+
+def load_iqon3000(args):
     root = pathlib.Path(args.data_dir)
 
     iqon_all = []
@@ -112,23 +129,42 @@ def main(args):
             if len(items) < args.min_set_size:
                 continue
             random.shuffle(items)
-            iqon_all.append({"set_id": set_id, "items": list(map(str, items))})
+            items = [{"name": str(item.name), "path": str(item), "category": -1} for item in items]
+            iqon_all.append({"set_id": set_id, "items": items})
+
+    return iqon_all
+
+
+def main(args):
+    random.seed(1219)
+
+    train_ratio, test_ratio = args.train_size, args.test_size
+    valid_ratio = 1 - (train_ratio + test_ratio)
+    assert train_ratio + valid_ratio + test_ratio == 1
+
+    if args.target == "shift15m":
+        iqon_all = load_shift15m(args)
+    elif args.target == "IQON3000":
+        iqon_all = load_iqon3000(args)
+    else:
+        raise ValueError("unknown dataset")
 
     train, valid_test = train_test_split(iqon_all, train_size=train_ratio, random_state=12)
     valid, test = train_test_split(valid_test, train_size=valid_ratio / (valid_ratio + test_ratio), random_state=19)
 
-    with open(root / "iqon_train.json", "w") as f:
+    root = pathlib.Path(args.data_dir)
+
+    with open(root / f"{args.target}_train.json", "w") as f:
         json.dump(train, f, indent=2)
-    with open(root / "iqon_valid.json", "w") as f:
+    with open(root / f"{args.target}_valid.json", "w") as f:
         json.dump(valid, f, indent=2)
 
     fitb = make_fitb_problems(test, args.n_candidates)
-
-    with open(root / "iqon_test_fitb.json", "w") as f:
+    with open(root / f"{args.target}_test_fitb.json", "w") as f:
         json.dump(fitb, f, indent=2)
 
     finbs = make_finbs_problems(test, args.n_candidates, args.n_mix, args.max_set_size_x, args.max_set_size_y)
-    with open(root / "iqon_test_finbs.json", "w") as f:
+    with open(root / f"{args.target}_test_finbs.json", "w") as f:
         json.dump(finbs, f, indent=2)
 
 
@@ -136,6 +172,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("--target", choices=["IQON3000", "shift15m"], help="target dataset name.")
     parser.add_argument("--data_dir", type=str, default="data", help="path to parent of IQON3000.")
     parser.add_argument("--min_set_size", type=int, default=4, help="minimun set size (int).")
     parser.add_argument("--n_candidates", type=int, default=4, help="the number of fitb candidates (int).")
